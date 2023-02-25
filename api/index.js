1a6e303a02520e5175448daff79d363f3de31641
unknown <herreraf.danilo@gmail.com>
2023-02-24 19:28:11 -0500

    modificacion backend

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+app  = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
+
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>console.log("DBConnection Successfull!"))
+.catch((err)=>{
+    console.log(err);
+});
+
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Backend server is running!");
+});
