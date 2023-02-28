5d9a31d283f7101fd2ef88fc39dc94ef12cb8984
unknown <herreraf.danilo@gmail.com>
2023-02-28 14:26:50 -0500

    routes order

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 
 
 dotenv.config();
@@ -22,6 +24,9 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running!");
