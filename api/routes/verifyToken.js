5b081f5728c0598d704e6bae273bc726e1eacee0
unknown <herreraf.danilo@gmail.com>
2023-02-27 20:44:03 -0500

    verifyToken

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) =>{
     if(authHeader){
         const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SEC, (err, user)=>{
-            if(err) return res.status(403).json("Token is not valid!");
+            if(err) res.status(403).json("Token is not valid!");
             req.user = user;
             next();
         })
@@ -25,4 +25,14 @@ const verifyTokenAndAuthorization = (req, res, next)=> {
     });
 };
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next)=> {
+    verifyToken(req, res, ()=> {
+        if(req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("You are not alowed to do that!");
+        }
+    });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin };
