2c4ebc3cfafb45ba0e33c138a5fde519163a9ad4
unknown <herreraf.danilo@gmail.com>
2023-03-01 18:36:02 -0500

    modificacion component products frontend

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,7 +1,8 @@
 import styled  from 'styled-components'
-import React from 'react'
-import { products } from '../data'
-import Product from './Product'
+import { useState, useEffect } from 'react'
+/* import { products } from '../data' */
+import Product from './Product';
+import axios from "axios";
 
 const Container = styled.div`
     padding: 20px;
@@ -10,10 +11,42 @@ const Container = styled.div`
     justify-content: space-between;
 `
 
-const Products = () => {
+const Products = ({cat, filters, sort}) => {
+
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(()=>{
+    const getProducts = async ()=> {
+      try {
+        const res = await axios.get( cat 
+          ? `http://localhost:5000/api/products?category=${cat}`
+          : "http://localhost:5000/api/products"
+        );
+        
+        setProducts(res.data);
+      } catch (error) {
+        
+      }
+    }
+    getProducts();
+  }, [cat])
+
+  useEffect(()=>{
+    cat && 
+    setFilteredProducts(
+      products.filter(item=> Object.entries(filters).every(([key, value])=>
+        item[key].includes(value)
+      )
+      )
+    );
+    
+  }, [products, cat, filters]);
+
+  console.log(filteredProducts)
   return (
     <Container>
-        {products.map((item)=>(
+        {filteredProducts.map((item)=>(
             <Product item={item} key={item.id}/>
         ))}
     </Container>
